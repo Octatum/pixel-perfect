29ba4805ffc95a100b1649733659e99e38566bef
Roberto Ruiz <rroberto.rruiz@gmail.com>
2018-07-24 13:43:23 -0500

    Background video working

--- src/pages/Home/DefaultSlide.jsx
+++ src/pages/Home/DefaultSlide.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 
-import { device } from '../../utils/device'
+import { device } from '../../utils/device';
 
 const Bg = styled.div.attrs({
   style: (props) => ({...props})
