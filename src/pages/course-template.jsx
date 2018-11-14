87c4bce4559165481780c18b2502a77b295156a0
fco <fcocarsor@gmail.com>
2018-11-14 11:45:33 -0600

    cambios en la pantalla de curso

--- src/pages/course-template.jsx
+++ src/pages/course-template.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import CoursePresentation from '../page-components/CoursePresentation';
+import AppLayout from '../components/AppLayout';
 
 const testData = {
   title: 'Matchmove',
   requirements: 'Basic knowledge of *3D software* (preferably *Autodesk Maya*)',
+  description: 'Allows to replicate the movement of a physical camera in live action shots in a 3D digital environment.',
   learnings: `
     In this course you’ll learn to use camera and object *tracking techniques*,
     and the interaction between the *3D tracking* and *modelling softwares*.
@@ -13,7 +15,11 @@ const testData = {
 };
 
 const CourseTemplate = () => {
-  return <CoursePresentation {...testData} />;
+  return (
+  <AppLayout>
+    <CoursePresentation {...testData} />
+  </AppLayout>
+  );
 };
 
 export default CourseTemplate;
